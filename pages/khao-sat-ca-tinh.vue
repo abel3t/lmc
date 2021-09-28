c03c2e54ba32fd693df67b297096f70c6cbbb26b
Abel Tran <abeltran.develop@gmail.com>
2021-09-28 21:14:27 +0700

    Update padding

--- pages/khao-sat-ca-tinh.vue
+++ pages/khao-sat-ca-tinh.vue
@@ -2,13 +2,13 @@
   <div>
     <QuestionBar v-bind:tab-question-type="tabQuestionType"/>
     <div class="questions">
-      <div v-for="question in questions" :key="question.id">
+      <div v-for="question in questions" :key="question.id" class="py-2">
         <span>Câu {{question.id}}:</span>
         <div class="inline-flex">
-          <div v-for="answer in question.answers" :key="answer.type">
+          <div v-for="answer in question.answers" :key="answer.type" class="ml-3">
             <label class="flex items-center">
               <t-checkbox name="options"/>
-              <span class="ml-2 text-sm">{{answer.text}}</span>
+              <span class="ml-2">{{answer.text}}</span>
             </label>
           </div>
         </div>
