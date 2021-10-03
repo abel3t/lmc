fb2dabc7fb30a1bffc459e9b0fd4e554a6e3ba72
Abel Tran <abeltran.develop@gmail.com>
2021-10-03 12:04:51 +0700

    Update rating

--- pages/an-tu-thuoc-linh/khao-sat.vue
+++ pages/an-tu-thuoc-linh/khao-sat.vue
@@ -1,20 +1,18 @@
 <template>
   <div>
     <QuestionBar v-bind:tab-question-type="tabQuestionType"/>
-    <div class="flex flex-col items-center">
+    <div>
       <div class="gift-test__questions">
         <div class="text-center text-3xl">Bài khảo sát ân tứ thuộc linh</div>
 
         <div v-for="(question, qIndex) in questions" :key="question.id" class="py-2">
-          <div><strong>Câu {{ question.id }}:</strong></div>
-          <div class="inline-flex">
-            <t-input :id="`q-${question.id}`" min="1" max="5" type="number" class="gift-test__questions_input"
-                     :variant="question.error ? 'danger': ''"
-                     @blur="updateMark(qIndex, $event)"
-                     @keyup.enter="updateMark(qIndex, $event)"
-                     :value="question.mark || ''"/>
-            <span class="ml-2 cursor-pointer" v-on:click="focusInput(`q-${question.id}`)">{{ question.text }}</span>
+          <div>
+            <strong>Câu {{ question.id }}:</strong>
+            <span class="ml-2 cursor-pointer">{{ question.text }}</span>
           </div>
+          <star-rating :animate="true" :rtl="false" :increment="0.5"
+                       :max-rating="10" :rating="question.mark || 0"
+                       @rating-selected="setRating(qIndex, $event)"></star-rating>
         </div>
 
         <div class="flex justify-center">
@@ -26,8 +24,27 @@
 </template>
 
 <style lang="scss">
+.vue-star-rating-pointer {
+  padding: 0;
+  margin: 0;
+}
+
+.vue-star-rating-star {
+  @media only screen and (min-width: 200px) {
+    height: 25px;
+    width: 25px;
+  }
+
+  @media only screen and (min-width: 600px) {
+    height: 40px;
+    width: 40px;
+  }
+}
+
 .gift-test__questions {
-  @apply border-0 shadow-2xl text-justify p-3 sm:p-4 md:p-5 lg:p-6 sm:w-full md:w-3/4 lg:w-2/3 my-5 bg-white;
+  @apply border-0 shadow-2xl text-justify p-4 sm:p-4 md:p-5 lg:p-6 sm:w-full md:w-3/4 lg:w-2/3 my-5 bg-white;
+  margin: 0 auto;
+
   &_input {
     width: 25px;
     height: 25px;
@@ -49,33 +66,33 @@
 </style>
 
 <script>
+import StarRating from 'vue-star-rating';
+
 import QuestionBar from '../../components/QuestionBar';
 import { TabQuestionType, UPDATE_GIFT_QUESTIONS, UPDATE_GIFT_RESULT } from '../../store';
 
 export default {
+  components: { QuestionBar, StarRating },
   data() {
     return {
       errorQuestionIds: {},
       tabQuestionType: TabQuestionType.Gift
     };
   },
-  components: { QuestionBar },
   computed: {
     questions: function () {
       return this.$store.getters.giftQuestions;
     }
   },
   methods: {
-    updateMark(qIndex, e) {
-      const value = parseInt(e.target?.value) || 0;
-
+    setRating(qIndex, rating) {
       const _questions = JSON.parse(JSON.stringify(this.questions));
 
-      const isValid = value > 0 && value <= 10;
+      const isValid = rating > 0 && rating <= 10;
 
       this.errorQuestionIds = { qIndex: isValid };
 
-      _questions[qIndex].mark = value;
+      _questions[qIndex].mark = rating;
       _questions[qIndex].error = !isValid;
 
       this.$store.dispatch(UPDATE_GIFT_QUESTIONS, _questions);
@@ -106,9 +123,6 @@ export default {
 
         window.open('/an-tu-thuoc-linh', '_self');
       }
-    },
-    focusInput(id) {
-      document.getElementById(id).focus();
     }
   },
   mounted() {
