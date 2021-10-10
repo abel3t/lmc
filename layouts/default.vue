bef89c2b5a7f861d5a1fcb6ee772e90b2cf77e30
Abel Tran <abeltran.develop@gmail.com>
2021-10-10 07:01:23 +0700

    Remove nuxtjs

--- layouts/default.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="sm:px-2 md:px-3 lg:px-6 bg-blue-100" style="min-height: 100vh">
-    <nuxt/>
-  </div>
-</template>
--- plugins/vue-chart.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import { Pie } from 'vue-chartjs'
-
-Vue.component('my-line', Pie.extend({
-  props: ['data', 'options'],
-  mounted () {
-    this.renderChart(this.data, this.options)
-  }
-}))
